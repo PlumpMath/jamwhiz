d5a4b63c6effae22c92332db48982a175acf2f7a
Yonatan Wolowelsky <grmmph@gmail.com>
2015-10-17 13:25:30 +0200

    server static folders

--- server.js
+++ server.js
@@ -8,3 +8,6 @@ var io = socket.listen(server);
 app.get('/', function(request, response){
   response.sendfile(__dirname + "/index.html");
 });
+
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/client"));
